c73258db5cde2db7ec7639ed5c40d4474d7a03b3
Arfannnnn <arfanmaulanaadam@gmail.com>
2025-07-16 22:18:16 +0700

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,8 +11,8 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Custom styles -->
-    <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
-    <script src="{{ asset('js/sb-admin-2.min.js') }}"></script>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+<link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
 
     <style>
         /* Styling untuk halaman */
@@ -118,6 +118,8 @@
     <!-- Vendor scripts -->
     <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
     <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<!-- Tambahkan JavaScript jika perlu -->
+<script src="{{ asset('js/app.js') }}"></script>
 
 </body>
 </html>
