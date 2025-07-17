434dce237802653e4eef043db92c81199293ed54
Arfannnnn <arfanmaulanaadam@gmail.com>
2025-07-17 14:20:29 +0700

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,72 +2,76 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register - Sistem Peminjaman fasilitas</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-    <!-- Fonts & Icons -->
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,400,600,700" rel="stylesheet">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+    <title>Register</title>
 
-    <!-- SB Admin 2 CSS -->
+    <!-- Custom fonts for this template -->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template -->
     <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
-
-    <!-- SweetAlert2 -->
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
 
-<body class="bg-gradient-primary">
+<body class="bg-gradient-primary d-flex align-items-center justify-content-center vh-100">
 
-    <div class="container">
-        <div class="row justify-content-center align-items-center min-vh-100">
-            <div class="col-xl-8 col-lg-10 col-md-9">
-                <div class="card shadow-lg border-0">
-                    <div class="card-body p-5">
-                        <div class="text-center">
-                            <h1 class="h4 text-gray-900 mb-4">Buat Akun Baru</h1>
-                        </div>
-
-                        @include('sweetalert::alert')
-
-                        @if ($errors->any())
-                            <div class="alert alert-danger">
-                                <ul class="mb-0">
-                                    @foreach ($errors->all() as $error)
-                                        <li>{{ $error }}</li>
-                                    @endforeach
-                                </ul>
-                            </div>
-                        @endif
-
-                        <form class="user" method="POST" action="{{ route('register_action') }}">
-                            @csrf
-
-                            <div class="form-group">
-                                <input type="text" class="form-control form-control-user" name="name" placeholder="Nama Lengkap" value="{{ old('name') }}" required>
+    <div class="container w-50">
+        <div class="card o-hidden border-0 shadow-lg">
+            <div class="card-body p-0">
+                @include('sweetalert::alert')
+                <div class="row justify-content-center">
+                    <div class="col-lg-10">
+                        <div class="p-5">
+                            <div class="text-center">
+                                <h1 class="h4 text-gray-900 mb-4">Creat<!-- DEBUG: REGISTER VIEW DIBACA -->
+unt!</h1>
                             </div>
 
-                            <div class="form-group">
-                                <input type="email" class="form-control form-control-user" name="email" placeholder="Email" value="{{ old('email') }}" required>
-                            </div>
-
-                            <div class="form-group row">
-                                <div class="col-sm-6 mb-3 mb-sm-0">
-                                    <input type="password" class="form-control form-control-user" name="password" placeholder="Password" required>
+                            @if($errors->any())
+                                <div class="alert alert-danger">
+                                    <ul>
+                                        @foreach($errors->all() as $error)
+                                            <li>{{ $error }}</li>
+                                        @endforeach
+                                    </ul>
                                 </div>
-                                <div class="col-sm-6">
-                                    <input type="password" class="form-control form-control-user" name="password_confirmation" placeholder="Ulangi Password" required>
+                            @endif
+
+                            <form method="POST" action="{{ route('register_action') }}">
+                                @csrf
+                                <div class="form-group">
+                                    <input type="text" class="form-control form-control-user"
+                                        placeholder="Name" name="name" value="{{ old('name') }}">
                                 </div>
+                                <div class="form-group">
+                                    <input type="email" class="form-control form-control-user"
+                                        placeholder="Email Address" name="email" value="{{ old('email') }}">
+                                </div>
+                                <div class="form-group row">
+                                    <div class="col-sm-6 mb-3 mb-sm-0">
+                                        <input type="password" class="form-control form-control-user"
+                                            placeholder="Password" name="password">
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <input type="password" class="form-control form-control-user"
+                                            placeholder="Repeat Password" name="password_confirmation">
+                                    </div>
+                                </div>
+                                <button type="submit" class="btn btn-primary btn-user btn-block">
+                                    Register Account
+                                </button>
+                            </form>
+
+                            <hr>
+                            <div class="text-center">
+                                <a class="small" href="{{ route('login') }}">Already have an account? Login!</a>
                             </div>
-
-                            <button type="submit" class="btn btn-primary btn-user btn-block">
-                                Daftar
-                            </button>
-                        </form>
-
-                        <hr>
-                        <div class="text-center">
-                            <a class="small" href="{{ route('login') }}">Sudah punya akun? Login!</a>
                         </div>
                     </div>
                 </div>
@@ -75,11 +79,6 @@
         </div>
     </div>
 
-    <!-- SB Admin 2 Scripts -->
-    <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
-    <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('vendor/jquery-easing/jquery.easing.min.js') }}"></script>
-    <script src="{{ asset('js/sb-admin-2.min.js') }}"></script>
 </body>
 
 </html>
